79aa91f4049d993e7e8b2e17d3c40bae31bbfc52
snackboycoder <xnetworksale@gmail.com>
2026-06-30 23:43:33 +0700

    Delete backup directory

--- backup/myconfig.sh
+++
@@ -1,4 +0,0 @@
-./configure --disable-everything --disable-x86asm --prefix=../ffmpeg_build \
-	--enable-shared --enable-static \
-	--enable-decoder=h264 --enable-parser=h264 --enable-demuxer=h264 \
-	--enable-muxer=mp4 --enable-protocol=file
